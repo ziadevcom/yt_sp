a3b131faf71ecc47e0b0263707f25f1314466465
= <=>
2023-03-18 14:37:23 +0500

    feature: Completed refresh access token feature

--- frontend/scripts/spotify.js
+++ frontend/scripts/spotify.js
@@ -1,7 +1,7 @@
 const SERVER_URL = "http://localhost:3000/";
 const SPOTIFY_BASE_URL = "https://api.spotify.com/v1";
 const connectSpotify = document.getElementById("connectSpotify");
-const spotifyProfile = document.getElementById("spotify-profile");
+const spotifyProfile = document.querySelector(".spotify-profile--content");
 
 // Redirect the user to Spotify login page when clicked on connect to spotify button
 connectSpotify.onclick = () => {
@@ -29,22 +29,24 @@ export async function getAccessToken() {
 }
 
 // Refresh Token Request to Server
-async function refreshAccessToken() {
-  // Send authorization code from spotify callback to server to get access token
-  const spotifyParams = new URLSearchParams(window.location.search);
-  const authrorizationCode = spotifyParams.get("code");
-  if (!authrorizationCode) return;
+export async function refreshAccessToken() {
+  const { refresh_token: refreshToken } = JSON.parse(
+    localStorage.getItem("accessToken")
+  );
 
+  // Send refresh access token request to the server
   const accessTokenJSON = await fetch(
-    `${SERVER_URL}spotify/refresh?authorizationCode=${authrorizationCode}&refreshToken=${refreshToken}`
+    `${SERVER_URL}spotify/refresh/${refreshToken}`
   );
   const accessTokenObj = await accessTokenJSON.json();
-  displaySpotifyUserUI(accessTokenObj);
+  accessTokenObj.refresh_token = refreshToken;
+  saveAccessToken(accessTokenObj);
   return accessTokenObj;
 }
 
 // Save access token in localStorage
 export function saveAccessToken(accessToken) {
+  accessToken.timestamp = Date.now();
   localStorage.setItem("accessToken", JSON.stringify(accessToken));
 }
 
@@ -52,15 +54,17 @@ export function saveAccessToken(accessToken) {
 export function getLocalAccessToken() {
   return JSON.parse(localStorage.getItem("accessToken")) || null;
 }
+
 export function displaySpotifyUserUI(userInfo) {
   // Add user's name and image in the UI
-  spotifyProfile.querySelector(
-    "h2"
-  ).innerText = `Welcome ${userInfo.display_name}`;
+  spotifyProfile.querySelector("h2 span").innerText = userInfo.display_name;
   spotifyProfile.querySelector("img").src = userInfo.images[0].url;
   spotifyProfile.style.display = "flex";
   // Hide the spotify authentication section
   connectSpotify.parentElement.style.display = "none";
+  // Hide Preloader
+  document.querySelector("#preloader").classList.add("hidden");
+  // document.querySelector("#preloader").style.display = "none";
 }
 
 // Get user information from spotify api and display user profile in UI
@@ -68,7 +72,6 @@ export async function getSpotifyUserInfo(accessToken) {
   if (accessToken.error) {
     return;
   }
-
   // Make request to spotify api to fetch user profile information
   const spotifyProfileJSON = await fetch(`${SPOTIFY_BASE_URL}/me`, {
     headers: {
@@ -76,5 +79,18 @@ export async function getSpotifyUserInfo(accessToken) {
     },
   });
   const spotifyProfileObj = await spotifyProfileJSON.json();
+
   return spotifyProfileObj;
+}
+
+// Check if access token has expired by comparing creation time and current time
+export function isExpiredAccessToken(accessToken) {
+  const creationTime = accessToken.timestamp / 1000;
+  const now = Date.now() / 1000;
+
+  if (now - creationTime >= 3000) {
+    return true;
+  }
+
+  return false;
 }
