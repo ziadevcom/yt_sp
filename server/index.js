32f7e3c5a1526c2c0b6bca0761779770fc7072b1
= <=>
2023-03-19 19:48:04 +0500

    feature: Added pagination support for youtube playlist endpoint

--- server/index.js
+++ server/index.js
@@ -1,134 +1,148 @@
-const cors = require("cors");
-const express = require("express");
-const app = express();
-const router = express.Router();
-app.use(cors());
+const cors = require('cors')
+const express = require('express')
+const app = express()
+const router = express.Router()
+app.use(cors())
 
 // Load Youtube API key
-require("dotenv").config();
+require('dotenv').config()
 
 // Load spotify app's credentials
-const clientId = process.env.CLIENT_ID;
-const clientSecret = process.env.CLIENT_SECRET;
-const redirectURI = process.env.REDIRECT_URI;
-console.log(redirectURI);
+const clientId = process.env.CLIENT_ID
+const clientSecret = process.env.CLIENT_SECRET
+const redirectURI = process.env.REDIRECT_URI
+
 // Get playlist endpoint
-router.get("/youtube/:id", async (req, res) => {
+router.get('/youtube/:id', async (req, res) => {
+  // Youtube api results max 50 results per api call
+  // API is paginated so we get next pageToken if there are more items
+  // In the code below, we call the youtube api again and again
+  // keep storing the results in an array
+  // and stop when number of results is equal to the number of total items
+  // in the playlist
+
+  let totalItems; let nextPageToken = ''
   try {
-    const json = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=999&playlistId=${req.params.id}&key=${process.env.API_KEY}`
-    );
-    let response = await json.json();
+    let response = null; let results = []
+    do {
+      response = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=999&playlistId=${req.params.id}&key=${process.env.API_KEY}&pageToken=${nextPageToken || ''}`
+      )
+      const data = await response.json()
+      totalItems = data.pageInfo.totalResults
+      nextPageToken = data.nextPageToken
+      results = results.concat(data.items)
+      // console.log(nextPageToken)
+      // console.log(results)
+    } while (results.length < totalItems)
 
-    // Error handling, in case api error, throw an error
-    if (response.error) {
-      console.log(response.error);
-      throw new Error("API Failure.");
-    }
-
+    // // Error handling, in case api error, throw an error
+    // if (response.error) {
+    //   console.log(response.error)
+    //   throw new Error('API Failure.')
+    // }
     // Format the response before returning
-    const items = response.items
-      .map((obj) => {
-        // Checking if video is private then return null so we can filter and remove these later.
-        if (
-          obj.snippet.title === "Private video" ||
-          obj.snippet.title === "Deleted video"
-        ) {
-          return null;
-        }
-        let thumbnail = obj.snippet.thumbnails.maxres;
-        console.log(thumbnail);
-        if (!thumbnail) {
-          thumbnail = obj.snippet.thumbnails.high;
-        }
-        return {
-          id: obj.snippet.resourceId.videoId,
-          title: obj.snippet.title,
-          thumbnail,
-        };
-      })
+    const items = results.map((obj) => {
+      // Checking if video is private then return null so we can filter and remove these later.
+      if (
+        obj.snippet.title === 'Private video' ||
+          obj.snippet.title === 'Deleted video'
+      ) {
+        return null
+      }
+      let thumbnail = obj.snippet.thumbnails.maxres
+      if (!thumbnail) {
+        thumbnail = obj.snippet.thumbnails.high
+      }
+      return {
+        id: obj.snippet.resourceId.videoId,
+        title: obj.snippet.title,
+        thumbnail
+      }
+    })
       .filter((item) => {
         if (!item) {
-          return false;
+          return false
         }
-        return true;
-      });
+        return true
+      })
 
     // Send json response of all videos
-    res.json(items);
+    res.json(items)
   } catch (error) {
-    res.json({ error: true, message: error.message });
+    console.log(error)
+    res.json({ error: true, message: error.message })
   }
-});
+})
 
 // Get user acess token spoitfy
-router.get("/spotify/:authorizationCode", async (req, res) => {
+router.get('/spotify/:authorizationCode', async (req, res) => {
   // authorization code from client
-  const authrorizationCode = req.params.authorizationCode;
+  const authrorizationCode = req.params.authorizationCode
   const formBody = new URLSearchParams({
-    grant_type: "authorization_code",
+    grant_type: 'authorization_code',
     code: authrorizationCode,
-    redirect_uri: redirectURI,
-  });
-  console.log(formBody);
+    redirect_uri: redirectURI
+  })
+  console.log(formBody)
   // Call spotify api to get access token
   // access token is used when calling spotify api on behalf of a user
   try {
-    const response = await fetch("https://accounts.spotify.com/api/token", {
-      method: "POST",
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
+        'Content-Type': 'application/x-www-form-urlencoded',
         Authorization:
-          "Basic " +
-          Buffer.from(clientId + ":" + clientSecret).toString("base64"), // converting client id and clientsecret to base64 becasue spotify demands that format
+          'Basic ' +
+          Buffer.from(clientId + ':' + clientSecret).toString('base64') // converting client id and clientsecret to base64 becasue spotify demands that format
       },
 
-      body: formBody, // body data type must match "Content-Type" header
-    });
+      body: formBody // body data type must match "Content-Type" header
+    })
 
     // Parses JSON response into native JavaScript objects
-    const json = await response.json();
-    res.json(json);
+    const json = await response.json()
+    res.json(json)
   } catch (e) {
-    res.json({ error: e.message });
+    res.json({ error: e.message })
   }
-});
+})
 
-app.get("/spotify/refresh/:refreshToken", async function (req, res) {
-  const refreshToken = req.params.refreshToken;
+app.get('/spotify/refresh/:refreshToken', async function (req, res) {
+  const refreshToken = req.params.refreshToken
 
   try {
     const authOptions = {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
+        'Content-Type': 'application/x-www-form-urlencoded',
         Authorization:
-          "Basic " +
-          Buffer.from(clientId + ":" + clientSecret).toString("base64"), // converting client id and clientsecret to base64 becasue spotify demands that format
+          'Basic ' +
+          Buffer.from(clientId + ':' + clientSecret).toString('base64') // converting client id and clientsecret to base64 becasue spotify demands that format
       },
       body: new URLSearchParams({
-        grant_type: "refresh_token",
-        refresh_token: refreshToken,
-      }),
-    };
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken
+      })
+    }
 
     // Post request to spotify api to get new access token
     const refreshedTokenJSON = await fetch(
-      "https://accounts.spotify.com/api/token",
+      'https://accounts.spotify.com/api/token',
       authOptions
-    );
+    )
 
-    const refreshedToken = await refreshedTokenJSON.json();
+    const refreshedToken = await refreshedTokenJSON.json()
 
-    console.log(refreshedToken);
+    console.log(refreshedToken)
 
     // Return the refreshed access token back to frontend
-    res.send(refreshedToken);
+    res.send(refreshedToken)
   } catch (error) {
-    res.send({ error: error.message });
+    res.send({ error: error.message })
   }
-});
+})
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000)
 
-app.use(router);
+app.use(router)
