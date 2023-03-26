f0907d44ce7d8e8e64c1f3d3bf4b74b29b895a8b
= <=>
2023-03-26 14:43:51 +0500

    feature: improved add to spotify popup

--- frontend/scripts/popup.js
+++ frontend/scripts/popup.js
@@ -1,12 +1,6 @@
 const popup = document.querySelector('#popup')
 const openPopup = document.querySelector('#open-popup')
 const closePopup = document.querySelector('#close-popup')
-const addToSpotifyForm = document.querySelector('#add-playlist-spotify')
-
-addToSpotifyForm.onsubmit = function (event) {
-  event.preventDefault()
-  // Todo
-}
 
 closePopup.onclick = togglePopup
 openPopup.onclick = togglePopup
